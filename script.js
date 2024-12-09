d6a4fe471199201956818f99f1cfa72bdae2217e
Eric Nwogu <reeko3d@gmail.com>
2024-12-09 01:41:48 +0000

    added suggestions to search input

--- script.js
+++ script.js
@@ -15,20 +15,31 @@ class Pokedex {
 
     this.baseStat = document.querySelectorAll('.base-stat');
 
+    this.suggestionDisplay = document.querySelector('.suggestions');
+    this.pokemons = [];
+
+    this.fetchAllPokemonNames()
+
     this.searchButton.addEventListener('click', () => {
       this.PokeAbilities.innerHTML = ''
       
       this.fetchPokedex()
-      this.searchInput.innerHTML = ''
-      this.baseStat.innerHTML = ''
+      this.searchInput.innerHTML = '';
+      this.baseStat.innerHTML = '';
+      this.suggestionDisplay.innerHTML = '';
     });
     document.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
-        this.PokeAbilities.innerHTML = ''
-        this.fetchPokedex()
-        this.searchInput.innerHTML = ''
-        this.baseStat.innerHTML = ''
+        this.PokeAbilities.innerHTML = '';
+        this.fetchPokedex();
+        this.searchInput.innerHTML = '';
+        this.baseStat.innerHTML = '';
+        this.suggestionDisplay.innerHTML = '';
       }
+    })
+
+    this.searchInput.addEventListener('input', () => {
+      this.handlePokemonNames()
     })
   }
 
@@ -87,7 +98,76 @@ class Pokedex {
     this.speedBaseStat.textContent = pokedexData.stats[5].base_stat;
 
   }
-}
+
+  async fetchAllPokemonNames() {
+
+    try {
+      const nameResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+
+      if (!nameResponse.ok) {
+        throw new Error('Unable to fetch Pokemon names')
+      }
+
+      const nameData = await nameResponse.json();
+
+      this.pokemons = nameData.results;
+
+      this.handlePokemonNames(this.pokemons)
+
+    }
+    catch(error) {
+      console.log('Unable to get pokemon names:', error)
+    }
+  }
+
+  handlePokemonNames() {
+
+    this.searchInput.addEventListener('keyup', () => {
+      let filteredPokemons = '';
+      let name = this.searchInput.value;
+
+      if (!name) {
+        this.suggestionDisplay.innerHTML = ''; 
+        return;
+      }
+
+      if (name.length) {
+        filteredPokemons = this.pokemons.filter((pokemon) => {
+          return pokemon.name.toLowerCase().includes(name.toLowerCase())
+        });
+        console.log(filteredPokemons)
+      }
+      this.displaySuggestions(filteredPokemons)
+
+      if (!filteredPokemons.length) {
+        this.suggestionDisplay.innerHTML = ''; 
+      }
+
+    })
+
+    }
+
+  displaySuggestions(filteredPokemons) {
+
+    const body = filteredPokemons.map((list) => {
+      return '<li onclick="selectInput(this)">' + list.name + '</li>'
+    });
+
+    this.suggestionDisplay.innerHTML = '<ul>' + body.join('') + '</ul>'
+
+    const suggestionItems = document.querySelectorAll('li');
+    suggestionItems.forEach((item) => {
+      item.addEventListener('click', () => {
+        this.selectInput(item.textContent);
+      })
+    })
+  }
+
+  selectInput(name) {
+    this.searchInput.value = name; 
+    this.suggestionDisplay.innerHTML = ''; 
+  }
+  }
 
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Pokedex();
